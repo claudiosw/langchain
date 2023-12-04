b6aff0f38e5f6576be0258dddf53aec0c0e70212
claudiosw <claudiosw@gmail.com>
2023-12-04 13:14:47 -0300

    improve: Modularizing basic langchain

--- 04_summarizing_with_langchain_openai/basic_prompt.py
+++ 04_summarizing_with_langchain_openai/basic_prompt.py
@@ -1,5 +1,5 @@
-from langchain.chat_models import ChatOpenAI
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from langchain_base import llm
 
 
 def get_summary_using_basic_prompt(text):
@@ -7,7 +7,6 @@ def get_summary_using_basic_prompt(text):
         SystemMessage(content='You are an expert copywriter with expertize in summarizing documents'),
         HumanMessage(content=f'Please provide a short and concise summary of the following text:\n TEXT: {text}')
     ]
-    llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo')
     if llm.get_num_tokens(text) <= 4000:
         summary_output = llm(messages)
         return summary_output.content
---
+++ 04_summarizing_with_langchain_openai/langchain_base.py
@@ -0,0 +1,4 @@
+from langchain.chat_models import ChatOpenAI
+
+
+llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo')
